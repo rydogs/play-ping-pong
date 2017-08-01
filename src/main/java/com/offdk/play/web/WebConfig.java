491dbc397fa1754a5098a309d41f1d6835f37467
rydogs <ryanyuliu@gmail.com>
2017-08-01 14:59:00 -0400

    Handle callback (#25)

--- src/main/java/com/offdk/play/web/WebConfig.java
+++ src/main/java/com/offdk/play/web/WebConfig.java
@@ -10,10 +10,12 @@ import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.guava.GuavaModule;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
@@ -29,7 +31,8 @@ class WebConfig extends WebMvcConfigurerAdapter {
   @Bean
   @Primary
   public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
-    return builder.modulesToInstall(new ParameterNamesModule(), new Jdk8Module(), new JavaTimeModule())
+    return builder.modulesToInstall(new ParameterNamesModule(JsonCreator.Mode.PROPERTIES),
+        new Jdk8Module(), new JavaTimeModule(), new GuavaModule())
       .serializationInclusion(JsonInclude.Include.NON_EMPTY)
       .featuresToEnable(JsonGenerator.Feature.WRITE_BIGDECIMAL_AS_PLAIN)
       .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS).build();
